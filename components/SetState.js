831f0c0080cbaabb5b487ddecf409c3d87ffbb39
mk-Guvi <61502453+mk-Guvi@users.noreply.github.com>
2020-05-16 16:18:55 +0530

    Setstates

--- components/SetState.js
+++ components/SetState.js
@@ -1,19 +1,30 @@
 import React, { Component } from "react"; //to use class import the {component} from react lib
 class counter extends Component {
-  constructor(){
-    super()
-this.state={
-  count: 0
-}
-Increament(){
-this.setState()=({
-  count: this.state.count+1
-})
-}
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+  increament() {
+    //this.state.count= this.state.count+1;
+    //console.log(this.state.count);
+    this.setState(
+      {
+        count: this.state.count + 1
+      },
+      () => console.log("call back value is", this.state.count)
+    ); //to overcome asynchronous execution cal back finction is used
+    console.log(this.state.count); //asynchronous execution
   }
   render() {
-    return(<div> <h1>Current Count: {this.state.count}</h1>
-    <button onClick={()=>this.Increament()}>Increament</button></div>)
+    return (
+      <div>
+        <h1>current count : {this.state.count}</h1>
+        <button onClick={() => this.increament()}>Increament</button>
+      </div>
+    );
   }
 }
 export default counter;
+s
